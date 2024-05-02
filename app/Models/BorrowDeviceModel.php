9dd15616eecc61d0ae16f2b18e9031142c9eabf6
Chompoonut Nuchyoo <125535532+kdr-it@users.noreply.github.com>
2024-05-02 16:10:36 +0700

    Initial commit

---
+++ app/Models/BorrowDeviceModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BorrowDeviceModel extends Model
+{
+    use HasFactory;
+}
